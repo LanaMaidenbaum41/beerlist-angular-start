51b676e3de3e5eac79e78733b04c33301435194d
Lana Maidenbaum <lanamaidenbaum@Lanas-MacBook-Pro.local>
2017-10-16 17:36:51 +0300

    getBeer and addBeer functioanlity

--- public/js/services/beerFactory.js
+++ public/js/services/beerFactory.js
@@ -3,17 +3,21 @@
  * removeBeer
  * getBeers
  */
-app.service('beersModel', function ($http) {
-    function addBeer() {
-
+app.service('beersService', function ($http) {    
+    function addBeer(newBeer) {
+        return $http.post('/beers', newBeer)
+            .then(function(response){
+                return angular.copy(response.data);
+            });
     };
 
     function removeBeer() {
-
+        
+        return beers
     };
 
     function getBeers() {
-        $http.get('/beers')
+       return $http.get('/beers')
             .then(function (response) {
                 //The function angular.copy makes a copy of the data received.
                 //It's good practice as it prevents any weird and unwanted object reference stuff.
