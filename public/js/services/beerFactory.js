04f27761124c3ff91f6285647487ac488a49620d
Lana Maidenbaum <lanamaidenbaum@Lanas-MacBook-Pro.local>
2017-10-18 09:20:46 +0300

    mess up

--- public/js/services/beerFactory.js
+++ public/js/services/beerFactory.js
@@ -3,26 +3,30 @@
  * removeBeer
  * getBeers
  */
-app.service('beersService', function ($http) {    
+
+app.service('beersService', function ($http) {
+    //ajax functions to connect with mongo database  
     function addBeer(newBeer) {
         return $http.post('/beers', newBeer)
-            .then(function(response){
+            .then(function (response) {
                 return angular.copy(response.data);
             });
     };
 
-    function removeBeer() {
-        
-        return beers
+    function removeBeer(beer,param) {
+        return $http.delete('/beers/' + param)
+            .then(function (response) {
+                return angular.copy(response.data);
+            });
     };
 
     function getBeers() {
-       return $http.get('/beers')
+        return $http.get('/beers')
             .then(function (response) {
                 //The function angular.copy makes a copy of the data received.
                 //It's good practice as it prevents any weird and unwanted object reference stuff.
                 return angular.copy(response.data);
-            });  
+            });
     };
 
     return {
